5b6fcffaac9c4db58b445db06d833bfcb341a7f4
austin <austinhuang1203@gmail.com>
2022-09-25 20:36:54 +0800

    adding form

--- code.js
+++ code.js
@@ -9,10 +9,28 @@ const additem = (()=> {
     }
 
     function create(title,description,duedate,priority,donyet=false){
-        return item(title,description,duedate,priority,donyet);
+        makeitems.append(item(title,description,duedate,priority,donyet));
+        makeitems.generate();
     }
 
     return {create};
+})();
+
+const makeitems = (()=>{
+    let itemlist = [];
+
+    function append(item){
+        itemlist.push(item);
+    }
+
+    function generate(){
+        for (let i = 0; i<itemlist.length; i++){
+
+        }
+    }
+
+    return {generate, append};
+
 })();
 
 const addproj =(()=> {
@@ -22,4 +40,57 @@ const addproj =(()=> {
         this.contents = [];
     }
 
+    function create(title){
+        proj(title);
+    }
+
+    return {create};
+
 })();
+
+const todoform = (()=> {
+
+    function open(){
+        document.getElementById("form1").style.visibility = "visible";
+        document.getElementById("popup1").style.visibility = "visible";
+    }
+
+    function close(){
+        document.getElementById("form1").style.visibility = "hidden";
+        document.getElementById("popup1").style.visibility = "hidden";
+    }
+
+    return{open,close};
+
+})();
+
+
+const initialise = (()=> {
+
+    function start(){
+        document.getElementById("openbutton").addEventListener("click",todoform.open);
+        document.getElementById("popup1").addEventListener("click",todoform.close);
+    }
+
+    return {start};
+
+})();
+
+
+
+
+
+
+
+// run at start
+initialise.start();
+
+
+
+
+
+
+
+
+
+
